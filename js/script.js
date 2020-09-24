4132178a2dce482cb690eca0f1f24222bc06d5c4
aiganov <man-simple@mail.ru>
2020-09-24 22:05:11 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -189,6 +189,60 @@ $('.reviews-slider').slick({
 
 });
 
+$('.team-slider').slick({
+	mobileFirst: true,
+	infinite: false,
+	dots: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	variableWidth: true,
+	centerMode: true,
+  
+
+
+	 responsive: [{
+
+      breakpoint: 768,
+      settings: {
+		infinite: false,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		centerMode: false,
+		variableWidth: false,
+      }
+      
+    }, {
+
+      breakpoint: 992,
+      settings: {
+          infinite: false,
+		  slidesToShow: 3,
+		  slidesToScroll: 1,
+		  centerMode: false,
+      	}
+
+    }, {
+
+      breakpoint: 1200,
+      settings: {
+          infinite: false,
+		  slidesToShow: 4,
+		  slidesToScroll: 1,
+		  centerMode: false,
+      	}
+
+    }, ]
+
+});
+
+
+
+
+
+
+
+
+
 /* Фильтр рубрик в Новостях */
 
 var removeRubrics = true;
